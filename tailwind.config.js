4e2d677cc6c7108f7c3b82ab642800226fb40763
Sharath-kumar-H-R <sharathkumarhr3@gmail.com>
2024-10-31 20:13:29 +0530

    Set up basic project structure for client-side with Tailwind CSS and added empty server and tests folders

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./client/src/**/*.html", "./client/src/js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
